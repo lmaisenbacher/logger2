be6e6076677a46f3c3976a9aef5ecd3fb2ccb31f
Fabian Schmid <fabian.schmid@mpq.mpg.de>
2017-11-13 20:54:58 +0100

    Initial commit.

---
+++ logger.py
@@ -0,0 +1,94 @@
+import sqlite3
+import configparser
+import pfeiffer, vacom
+import time
+from serial import SerialException
+import datetime
+
+updateInteval = 1000 # Update interval in ms
+dbpath = "database.sqlite"
+configpath = "config.ini"
+
+conn = sqlite3.connect(dbpath)
+
+c = conn.cursor()
+
+conf = configparser.ConfigParser()
+conf.read(configpath)
+
+devices = []
+
+def writeValue(deviceID, deviceChannel, time, value, measurementType, name, longName, Unit):
+    """Write a new measured value to the database
+    :param deviceID: ID of the measurement device
+    :param deviceChannel: Channel of the measurement device
+    :param time: Timestamp in ISO format
+    :param value: Measured value
+    :param measurementType: Type of the measurement
+    :param name: Short name of the measured value
+    :param longName: Long name of the measured value
+    :param unit: Unit of the measured value
+    """
+    c.execute("INSERT INTO Measurements (DeviceID, DeviceChannel, Time, Value, Type, Name, LongName, Unit) VALUES (?,?,?,?,?,?,?,?)", (deviceID, deviceChannel, time, value, measurementType, name, longName, unit))
+    conn.commit()
+
+def checkDeviceExists(name, address, deviceType, model):
+    """Check if a device with specified name, address and type already exists in the database.
+    :param name: Device name
+    :param address: Device address
+    :param deviceType: Device type
+    :param model: Device model
+    :returns: -1 if the device is not found in the database, the deviceID of the first matching device otherwhise
+    """
+    c.execute("SELECT * FROM Devices WHERE Name = ? AND Address = ? AND Type = ? AND Model = ?", (name, address, deviceType, model))
+    res = c.fetchall()
+    if(len(res)) > 0:
+        return res[0][0]
+    else:
+        return -1
+
+def addDevice(name, address, deviceType, model):
+    """Add a new device to the database.
+    :param name: Device name
+    :param address: Device address
+    :param deviceType: Device type
+    :param model: Device model
+    """
+    c.execute("INSERT INTO Devices (Name, Address, Type, Model) VALUES (?,?,?,?)", (name, address, deviceType, model))
+    conn.commit()
+
+def readConfig():
+    for section in conf.sections():
+        name = conf[section]["name"]
+        address = conf[section]["address"]
+        deviceType = conf[section]["type"]
+        model = conf[section]["model"]
+
+        if(model == "VACOM COLDION CU-100"):
+            try:
+                deviceObject = vacom.CU100(port=address)
+            except SerialException as err:
+                print("Could not open serial device: {}".format(err))
+                continue
+        elif(model == "Pfeiffer Vacuum TPG 261"):
+            try:
+                deviceObject = vacom.CU100(port=address)
+            except SerialException as err:
+                print("Could not open serial device: {}".format(err))
+                continue
+
+        deviceID = checkDeviceExists(name, address, deviceType, model)
+        if(deviceID == -1):
+            deviceID = addDevice(name, address, deviceType, model)
+
+        devices.append({"object":deviceObject, "ID":deviceID})
+
+if __name__ == "__main__":
+    readConfig()
+    while(True):
+        for device in devices:
+            value = device["object"].pressure_gauge()[0]
+            timestamp = datetime.datetime.now().isoformat()
+            writeValue(device["ID"], 1, timestamp, value, "TEMP", "Test", "Test", "mBar")
+
+        time.sleep(updateInterval)
---
+++ vacom.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+
+"""This module contains drivers for the following equipment from Vacom
+
+COLDION CU-100 cold cathode ionization gauge controller
+
+"""
+
+import time
+import serial
+
+class CU100(object):
+
+    def __init__(self, port='/dev/ttyUSB0', baudrate=19200):
+        """Initialize internal variables and serial connection
+
+        :param port: The COM port to open. See the documentation for
+            `pyserial <http://pyserial.sourceforge.net/>`_ for an explanation
+            of the possible value. The default value is '/dev/ttyUSB0'.
+        :type port: str or int
+        :param baudrate: 9600, 19200, 38400, 57600, 115200 where 19200 is the default
+        :type baudrate: int
+        """
+        # The serial connection should be setup with the following parameters:
+        # 1 start bit, 8 data bits, No parity bit, 1 stop bit, no hardware
+        # handshake. These are all default for Serial and therefore not input
+        # below
+        self.serial = serial.Serial(port=port, baudrate=baudrate, timeout=1)
+
+    def pressure_gauge(self):
+        """Return the pressure measured by the gauge
+
+        :return: (pressure value in mbar, (0, "NONE"))
+        """
+        command = [0xA5, 0x50, 0x00, 0x00, 0x20, 0x10, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+        self.serial.write(self.command_with_checksum(command))
+        time.sleep(0.1)
+        response = self.serial.read(self.serial.inWaiting())
+        value = float(response[6:22].decode("ASCII").rstrip("\0"))
+
+        return value, (0, "NONE")
+
+    def crc16_update(self, crc, a):
+        """Cyclical calculation of a CRC-16 checksum.
+        
+        :param crc: The previous checksum value
+        :param a: The byte to be added to the checksum
+        :returns: The new CRC-16 checksum
+        """
+        crc = crc^a
+
+        for i in range(8):
+            if (crc & 1):
+                crc = (crc >> 1)^0xA001
+            else:
+                crc = crc >> 1
+        return crc
+
+    def command_with_checksum(self, command):
+        """Adds a CRC-16 checksum to a command.
+
+        :param command: The command as an array of bytes
+        :returns: The command including checksum as an array of bytes
+        """
+        crc = 0xFFFF
+        for element in command:
+            crc = self.crc16_update(crc, element)
+
+        newCommand = list(command)
+        newCommand.append(crc&0xFF)
+        newCommand.append(crc>>8)
+
+        return newCommand
