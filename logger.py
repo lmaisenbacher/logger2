45d42dc1e17e88c1f3cfc5293066c463d667ba66
Fabian Schmid <fabian.schmid@mpq.mpg.de>
2017-12-12 19:03:50 +0100

    Code cleanup.

--- logger.py
+++ logger.py
@@ -1,14 +1,19 @@
 import sqlite3
-import configparser, json
-import pfeiffer, vacom, keithley
-import time
-from serial import SerialException
+import configparser
+import json
 import datetime
+import time
 
-configpath = "config.ini"
+from serial import SerialException
+
+import pfeiffer
+import vacom
+import keithley
+
+CONFIGPATH = "config.ini"
 
 conf = configparser.ConfigParser()
-conf.read(configpath)
+conf.read(CONFIGPATH)
 
 dbpath = conf["Database"]["path"]
 updateInterval = int(conf["Update"]["interval"])
@@ -18,15 +23,15 @@ deviceConfigPath = conf["Devices"]["configpath"]
 conn = sqlite3.connect(dbpath, detect_types=sqlite3.PARSE_DECLTYPES)
 c = conn.cursor()
 
-def writeValue(deviceID, channelID, time, value):
+def writeValue(deviceID, channelID, timestamp, value):
     """Write a new measured value to the database
 
     :param deviceID: ID of the measurement device
     :param channelID: ID of the measurement channel
-    :param time: Timestamp as datetime object
+    :param timestamp: Timestamp as datetime object
     :param value: Measured value
     """
-    c.execute("INSERT INTO Measurements (DeviceID, ChannelID, Time, Value) VALUES (?,?,?,?)", (deviceID, channelID, time, value))
+    c.execute("INSERT INTO Measurements (DeviceID, ChannelID, Time, Value) VALUES (?,?,?,?)", (deviceID, channelID, timestamp, value))
     conn.commit()
 
 def checkDeviceExists(name, address, deviceType, model):
@@ -40,7 +45,7 @@ def checkDeviceExists(name, address, deviceType, model):
     """
     c.execute("SELECT * FROM Devices WHERE Name = ? AND Address = ? AND Type = ? AND Model = ?", (name, address, deviceType, model))
     res = c.fetchall()
-    if(len(res)) > 0:
+    if res:
         return res[0][0]
     else:
         return -1
@@ -69,7 +74,7 @@ def checkChannelExists(deviceID, deviceChannel, measurementType, shortName, long
     """
     c.execute("SELECT * FROM Channels WHERE DeviceID = ? AND DeviceChannel = ? AND Type = ? AND ShortName = ? AND LongName = ? AND Unit = ?", (deviceID, deviceChannel, measurementType, shortName, longName, unit))
     res = c.fetchall()
-    if(len(res)) > 0:
+    if res:
         return res[0][0]
     else:
         return -1
@@ -83,26 +88,29 @@ def addChannel(deviceID, deviceChannel, measurementType, shortName, longName, un
     :param longName: Full description of the channel
     :param unit: Unit of the measurement
     """
-    c.execute("INSERT INTO Channels (DeviceID, DeviceChannel, Type, ShortName, LongName, Unit) VALUES (?,?,?,?,?,?)",(deviceID, deviceChannel, measurementType, shortName, longName, unit))
+    c.execute("INSERT INTO Channels (DeviceID, DeviceChannel, Type, ShortName, LongName, Unit) VALUES (?,?,?,?,?,?)", (deviceID, deviceChannel, measurementType, shortName, longName, unit))
     conn.commit()
 
-def initDevices(devices):  
-  
+def initDevices(devices):
+    """Initialize the device objects
+
+    :param devices: List of the devices to initialize
+    """
     for device in devices:
         print("Trying to open device {}".format(device["Name"]))
-        if(device["Model"] == "Pfeiffer Vacuum TPG 261"):
+        if device["Model"] == "Pfeiffer Vacuum TPG 261":
             try:
                 device["Object"] = pfeiffer.TPG261(device["Address"])
             except SerialException as err:
                 print("Could not open serial device: {}".format(err))
                 continue
-        if(device["Model"] == "VACOM COLDION CU-100"):
+        if device["Model"] == "VACOM COLDION CU-100":
             try:
                 device["Object"] = vacom.CU100(device["Address"])
             except SerialException as err:
                 print("Could not open serial device: {}".format(err))
                 continue
-        if(device["Model"] == "Keithley 2001 Multimeter"):
+        if device["Model"] == "Keithley 2001 Multimeter":
             device["Object"] = keithley.Multimeter(device["Address"])
             # Set up device parameters
             device["Object"].setNrMeasurements(device["NAverages"])
@@ -110,14 +118,14 @@ def initDevices(devices):
             device["Object"].setNrPLCycles(device["NPLC"])
 
         deviceID = checkDeviceExists(device["Name"], device["Address"], device["Type"], device["Model"])
-        if(deviceID == -1):
+        if deviceID == -1:
             addDevice(device["Name"], device["Address"], device["Type"], device["Model"])
             deviceID = checkDeviceExists(device["Name"], device["Address"], device["Type"], device["Model"])
 
         device["ID"] = deviceID
         for channel in device["Channels"]:
             channelID = checkChannelExists(deviceID, channel["DeviceChannel"], channel["Type"], channel["ShortName"], channel["LongName"], channel["Unit"])
-            if(channelID == -1):
+            if channelID == -1:
                 addChannel(deviceID, channel["DeviceChannel"], channel["Type"], channel["ShortName"], channel["LongName"], channel["Unit"])
                 channelID = checkChannelExists(deviceID, channel["DeviceChannel"], channel["Type"], channel["ShortName"], channel["LongName"], channel["Unit"])
             channel["ID"] = channelID
@@ -126,13 +134,13 @@ if __name__ == "__main__":
     with open(deviceConfigPath) as deviceConfig:
         devices = json.load(deviceConfig)
     initDevices(devices)
-    while(True):
+    while True:
         for device in devices:
             print(device["Name"])
             for channel in device["Channels"]:
                 print(channel["ShortName"])
                 value = device["Object"].getValue(channel["DeviceChannel"])
-                if("Multiplier" in channel):
+                if "Multiplier" in channel:
                     value *= channel["Multiplier"]
                 timestamp = datetime.datetime.now()
                 print("{}\t{}".format(timestamp, value))
