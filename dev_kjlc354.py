a8cdb7d93ce1f9709a384c62ef9c18a5945aa5bc
Lothar Maisenbacher <lothar.maisenbacher@berkeley.edu>
2022-11-29 14:44:31 -0800

    Added error handling.

--- dev_kjlc354.py
+++ dev_kjlc354.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 This module contains drivers for the Kurt J. Lesker KJLC 354 series ion pressure gauge.
-It implements the serial protocol of the gauge over a RS-485 port
+It implements the serial protocol of the gauge over a two-wire RS-485 port
 (not to be confused with a RS-232 port, which uses the same 9-pin sub-D connector)
 to read out the pressure in units of Torr.
 """
@@ -31,21 +31,41 @@ class Device(dev_generic.Device):
                 **device.get('SerialConnectionParams', {}))
         except serial.SerialException:
             raise LoggerError(
-                f"Serial connection with {device['Device']} couldn't be opened")
+                f"Serial connection on port {device['Address']} couldn't be opened")
 
-    def read_pressure(self):
-        query = f'#{self.device["DeviceSpecificParams"]["InternalAddress"]}RD\r'.encode("ASCII")
+    def query(self, command):
+        """Query device with command `command` (str) and return response."""
+        internal_address = self.device["DeviceSpecificParams"]["InternalAddress"]
+        query = f'#{internal_address}{command}\r'.encode("ASCII")
         n_write_bytes = self.connection.write(query)
         if n_write_bytes != len(query):
-            raise LoggerError(f"Failed to query {self.device['Device']}")
+            raise LoggerError("Failed to write to device")
         rsp = self.connection.readline()
-        if rsp.decode()[0] == '?':
-            raise LoggerError(f"Received an error response from {self.device['Device']}")
-        elif rsp.decode()[0] == '*':
-            # Convert from base ten scientific notation to floating point
-            return float(rsp[4:7]) * 10 ** float(rsp[9:11])
-        else:
-            raise LoggerError(f"Didn't receive acknowledgement from {self.device['Device']}")
+        try:
+            rsp = rsp.decode()
+        except UnicodeDecodeError:
+            raise LoggerError(f"Error in decoding response ('{rsp}') received")
+        if rsp == '':
+            raise LoggerError(
+                "No response received")
+        if rsp.startswith("?"):
+            raise LoggerError(
+                f"Received an error response: '{rsp}'")
+        if not rsp.startswith(f"*{internal_address} "):
+            raise LoggerError(
+                f"Didn't receive correct acknowledgement (response received: '{rsp}')")
+        return rsp[4:]
+
+    def read_pressure(self):
+        """Read pressure."""
+        # Check whether filament is powered up and gauge is reading
+        rsp = self.query("IGS")
+        if rsp.startswith("0"):
+            raise LoggerError(
+                "Filament is not powered up, no pressure reading available")
+        # Read pressure
+        rsp = self.query("RD")
+        return float(rsp)
 
     def get_values(self):
         """Read channels."""
