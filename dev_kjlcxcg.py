22858a35c17a5dfe01a17d6e6f16d85f038d76d0
Jack Mango <jackmango@optonline.net>
2023-04-05 12:00:05 -0700

    Switched form busio package to adafruit supported package

--- dev_kjlcxcg.py
+++ dev_kjlcxcg.py
@@ -3,13 +3,11 @@
 Write a docstring here
 """
 
-import serial
 import logging
-import board
-import busio
 import os
 import adafruit_ads1x15.ads1015 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
+from adafruit_extended_bus import ExtendedI2C as I2C
 
 
 import dev_generic
@@ -33,7 +31,7 @@ class Device(dev_generic.Device):
             self.channels =self.device["Channels"]    
             for name, info in self.channels:
                 if info["I2CAddress"] not in self.ads:
-                    self.ads[info["I2CAddress"]] = ADS.ADS1115(busio.I2C(), address=int(info["I2CAddress"], 16), data_rate=860)
+                    self.ads[info["I2CAddress"]] = ADS.ADS1115(I2C(), address=int(info["I2CAddress"], 16), data_rate=860)
                 info["ChanObj"] = ({name: AnalogIn(self.ads["I2CAddress"], info["Pins"]["Signal"], info["Pins"]["Reference"])})         
         except os.OSError:
             raise LoggerError(
