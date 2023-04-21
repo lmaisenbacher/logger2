b317da905f2feb7f6bf1d64fe4b89accd534c1d9
Jack Mango <jackmango@optonline.net>
2023-04-21 15:48:24 -0700

    added support for OLED display

--- dev_kjlcxcg.py
+++ dev_kjlcxcg.py
@@ -32,6 +32,7 @@ class Device(dev_generic.Device):
             for channel_id, channel in self.channels.items():
                 analog_channel = ADS.ADS1115(busio.I2C(board.SCL, board.SDA), address=int(channel["I2CAddress"], 16))
                 channel["ChanObj"] = AnalogIn(analog_channel, channel["Pins"]["Signal"], channel["Pins"]["Reference"])
+                self.init_display()
         except OSError:
             raise LoggerError(
                 f"I2C port for {device['Address']} couldn't be opened")
@@ -44,11 +45,12 @@ class Device(dev_generic.Device):
     def get_values(self):
         """Read channels."""
         readings = {}
-        for channel_id, channel in self.channels.items():
+        for chan_num, (channel_id, channel) in enumerate(self.channels.items()):
             if channel['Type'] in ['Pressure']:
                 try:
                     value = self.read_pressure(channel['ChanObj'])
                     readings[channel_id] = value
+                    self.update_display(value, chan_num)
                 except OSError:
                     raise LoggerError(
                         f"I2C port ({channel['I2CAddress']}) for {channel_id} couldn't be opened")
@@ -57,3 +59,43 @@ class Device(dev_generic.Device):
                     f'Unknown channel type \'{channel["Type"]}\' for channel \'{name}\''
                     +f' of device \'{self.device["Device"]}\'')
         return readings
+
+    def clear_display(self):
+        """Change all pixels to black on the display."""
+        self.display.fill(0)
+        self.display.show()
+        return
+
+    def init_display(self):
+        """Establish a connection to the display for later use and add labels for measurements."""
+        self.display = adafruit_ssd1306.SSD1306_I2C(128, 64, board.I2C(), addr=0x3D, reset=digitalio.DigitalInOut(board.D4))
+        self.clear_display()
+        self.image = Image.new("1", (self.dispaly.width, self.display.height))
+        self.draw = ImageDraw.Draw(self.image)
+        self.font = ImageFont.load_default()
+        for i, channel_id in enumerate(self.channels.keys()):
+            text = f"{channel_id}:"
+            self.draw.text(
+            (0, i * self.display.width // 4),
+            text,
+            font=self.font,
+            fill=255,
+            )
+        return
+    
+    def update_display(self, value, channel_num):
+        """
+            Update the display with current pressure gauge readings in Torr for the channel with channel_num.
+            Readings appear in the middle of the screen
+            
+            channel_num : int
+                Channel number based on order in channel.keys().
+        """
+        text = f"{value} Torr"
+        self.draw.text(
+            (self.display // 2, channel_num * self.height // 4),
+            text,
+            font=self.font,
+            fill=255,
+        )
+        return
