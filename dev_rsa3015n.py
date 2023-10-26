ff21a558226c87f63ad6a2dabc41f834bc5ec258
Unitrap <unitrap@berkeley.edu>
2023-10-25 18:08:16 -0700

    testing branching

--- dev_rsa3015n.py
+++ dev_rsa3015n.py
@@ -7,3 +7,11 @@ import numpy as np
 
 from amodevices import dev_generic
 from amodevices.dev_exceptions import DeviceError
+
+import pyvisa
+
+rm = pyvisa.ResourceManager()
+rm.list_resources()
+# ('ASRL1::INSTR', 'ASRL2::INSTR', 'GPIB0::12::INSTR')
+inst = rm.open_resource('GPIB0::12::INSTR')
+print(inst.query("*IDN?"))
