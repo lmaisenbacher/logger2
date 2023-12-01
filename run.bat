79e94253797fcfd4dd334320056178848ba03ede
Unitrap @ UC Berkeley <unitrap@berkeley.edu>
2023-11-30 17:02:11 -0800

    Adjusted batch file such that it can be run as a Windows service.

--- run.bat
+++ run.bat
@@ -1,1 +1,5 @@
-pipenv run python logger.py
+:: Initialize pipenv virtual environment. To find the path `<PATH_TO_VENV>`, use `pipenv --venv` in the directory of the virtual environment.
+call <PATH_TO_VENV>\Scripts\activate.bat
+:: Run the logger. Replace `<PATH_TO_LOGGER_REPO>` with the path to the directory of the `logger2` repo, and `<PATH_TO_CONFIG_FILE>` with the path the "config.ini" file.
+cd <PATH_TO_LOGGER_REPO>
+call python logger.py -c <PATH_TO_CONFIG_FILE>
